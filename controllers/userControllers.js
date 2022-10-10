04413a822e1024ac81dbf5a57f4016369bc80841
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-10-10 17:00:30 +1030

    added friend functions in user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -69,5 +69,38 @@ module.exports = {
     },
 
     // ADD friend
+    addFriend(req, res) {
+        console.log('You are adding a new friend');
+        console.log(req.body);
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friend: req.body }},
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res
+                    .status(404)
+                    .json({ message: 'No user found with that ID' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));   
+    },
+
     // DELETE friend
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res
+                    .status(404)
+                    .json({ message: 'No user found with that ID' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 }
