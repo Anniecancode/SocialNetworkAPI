326931a755efa94f4cac2c06d093cb4bc3314a05
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-10-10 21:14:59 +1030

    created seed

---
+++ utils/data.js
@@ -0,0 +1,11 @@
+const userData = [
+    { username: 'Annie', email: 'annie@gmail.com' },
+    { username: 'Amy', email: 'amy123@email.com' },
+    { username: 'Diva', email: 'divaa@hotmail.com' },
+];
+
+const thoughtData = [
+    { thoughtText: 'Got headache cant think straight', username: 'Annie' },
+    { thoughtText: 'Wanna recommand this place', username: 'Amy' },
+    { thoughtText: 'Wow that post was fun', username: 'Diva' },
+]
