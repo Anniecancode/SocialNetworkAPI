b14816f445044bfb4d4a4e3475a8f55a88ecb76c
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-10-10 16:50:37 +1030

    added thought controller

---
+++ controllers/thoughtControllers.js
@@ -0,0 +1,94 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+
+    // GET all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+        //.select('-__v')
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // GET a single thought
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .populate({
+            path: 'reactions',
+            select: '-__v',
+        })
+        .select('-__v')
+        .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: 'No thought with that ID' })
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // CREATE a thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thought._id }},
+                { new: true }
+            )
+        })
+        .then((user) => 
+            !user
+                ? res.status(404).json({
+                    message: 'Thought created, but found no user with that ID',
+                })
+                // json(thought?)
+                : res.json('Created the thought 🎉')
+        )
+        .catch((err) => {
+            console.log(err)
+            return res.status(500).json(err)
+        });
+    },
+
+    // UPDATE a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: 'No thought with that ID' })
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // DELETE a thought
+    deleteThought(req, res) {
+        Thought.finsOneAndDelete({ _id: req.params.thoughtId })
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: 'No thought with that ID' })
+                : User.findOneAndUpdate(
+                    { thoughts: req.params.thoughtId },
+                    { $pull: { thoughts: req.params.thoughtId } },
+                    { new: true }
+                )
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({
+                    message: 'Thought created but no user with that ID!',
+                })
+                // json(thought?)
+                : res.json({ message: 'Thought successfully deleted!' })
+        )
+        .catch((err) => res.status(500).json(err));
+    },   
+
+    // CREATE reaction
+    // DELETE reaction
+
+}
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,6 +2,7 @@ const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
 module.exports = {
+
     // GET all users
     getUsers(req, res) {
         User.find()
@@ -50,8 +51,8 @@ module.exports = {
                 : res.json(user)
         )
         .catch((err) => {
-              console.log(err);
-              res.status(500).json(err);
+            console.log(err);
+            res.status(500).json(err);
         });
     },
 
@@ -59,9 +60,9 @@ module.exports = {
     deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
         .then((user) =>
-        !user
-            ? res.status(404).json({ message: 'No user with that ID' })
-            : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : Thought.deleteMany({ _id: { $in: user.thoughts } })
         )
         .then(() => res.json({ message: 'User and thoughts deleted!' }))
         .catch((err) => res.status(500).json(err));
